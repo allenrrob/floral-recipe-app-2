c90d704dfd8001157886f1c9e973990ef397d586
allen <allenrrob@gmail.com>
2026-09-08 20:25:45 -0700

    updated app.py to link models

--- app.py
+++ app.py
@@ -6,18 +6,25 @@ from datetime import datetime, timezone
 
 # Floral Recipe App 
 app = Flask(__name__)
-Scss(app)
 
+# Configure SQLite Database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 
+# Initialize extensions
 db = SQLAlchemy(app)
+Scss(app, static_folder='static', asset_folder='static/scss')
+
+# Import models so SQLAlchemy is aware of them
+import models 
 
 @app.route('/')
 def index():
     return "Floral Recipe App is running!"
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() # Auto-creates app.db and tables if they don't exist
     app.run(debug=True)
 
 

62679b1244386d8a106ffbd1c1323e909a72bf2f
allenrrob <allenrrob@gmail.com>
2026-09-21 15:08:23 -0700

    added edit ingredients types in app.py

--- app.py
+++ app.py
@@ -326,16 +326,36 @@ def design_fees():
 @app.route('/ingredient-types', methods=['GET', 'POST'])
 def ingredient_types():
     if request.method == 'POST':
-        name = request.form.get('name')
-        markup = request.form.get('markup')
-        if name and markup:
-            new_type = models.IngredientType(name=name, markup=float(markup))
-            db.session.add(new_type)
-            db.session.commit()
-            return redirect('/ingredient-types')
+        action = request.form.get('action')
 
-    all_types = models.IngredientType.query.all()
-    return render_template('ingredient_types.html', types=all_types)
+        # Action: Create an new ingredient type
+        if action == 'create_type':
+            name = request.form.get('name')
+            markup = request.form.get('markup')
+            if name and markup:
+                new_type = models.IngredientType(name=name, markup=float(markup))
+                db.session.add(new_type)
+                db.session.commit()
+                return redirect('/ingredient-types')
+
+        # Action: Edit existing ingredient type
+        elif action == 'edit_type':
+            type_id = request.form.get('type_id')
+            name = request.form.get('name')
+            markup = request.form.get('markup')
+
+            if type_id and name and markup:
+                type = models.IngredientType.query.get(int(type_id))
+                if type:
+                    type.name = name.strip()
+                    type.markup = float(markup)
+
+                    db.session.commit()
+
+            return redirect('/ingredient-types')
+        
+        all_types = models.IngredientType.query.order_by(models.IngredientType.name.asc()).all()
+        return render_template('ingredient_types.html', types=all_types)
 
 # --- Vendor Manager ---
 @app.route('/vendors', methods=['GET', 'POST'])

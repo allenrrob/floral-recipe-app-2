68b05e753c16513ee86afa694589228ac0397f08
allenrrob <allenrrob@gmail.com>
2026-09-21 00:05:08 -0700

    added code to edit vendor in vendors.html

--- app.py
+++ app.py
@@ -341,14 +341,31 @@ def ingredient_types():
 @app.route('/vendors', methods=['GET', 'POST'])
 def vendors():
     if request.method == 'POST':
-        name = request.form.get('name')
-        if name:
-            new_vendor = models.Vendor(name=name)
-            db.session.add(new_vendor)
-            db.session.commit()
+        action = request.form.get('action')
+
+        # Action: Create a new vendor
+        if action == 'create_vendor':
+            name = request.form.get('name')
+            if name:
+                new_vendor = models.Vendor(name=name)
+                db.session.add(new_vendor)
+                db.session.commit()
+                return redirect('/vendors')
+
+        elif action == 'edit_vendor':
+            vendor_id = request.form.get('vendor_id')
+            name = request.form.get('name')
+
+            if vendor_id and name:
+                vendor = models.Vendor.query.get(int(vendor_id))
+                if vendor:
+                    vendor.name = name.strip()
+
+                    db.session.commit()
             return redirect('/vendors')
 
-    all_vendors = models.Vendor.query.all()
+
+    all_vendors = models.Vendor.query.order_by(models.Vendor.name.asc()).all()
     return render_template('vendors.html', vendors=all_vendors)
 
 # --- Profit Margin Dashboard ---
